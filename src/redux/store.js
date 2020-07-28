65ea5f5727003bd896c81fe0016e4c6310ce78e5
TheAkindele <akindelemuhyideen@gmail.com>
2020-07-28 17:00:50 +0100

    redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,11 +8,3 @@ const middlewares = [thunk]
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
 export default store
-
-// https://image.tmdb.org/t/p/w154/1Duc3EBiegywczxTWekvy03HWai.jpg
-
-// https://api.themoviedb.org/3/configuration?api_key=37ad0cf97af435a0a03c6e3da5f19502
-
-// imageURL = `https://image.tmdb.org/t/p/${poster_path}`
-
-// video =  https://api.themoviedb.org/3/movie/297762/videos?api_key=###&language=en-US
