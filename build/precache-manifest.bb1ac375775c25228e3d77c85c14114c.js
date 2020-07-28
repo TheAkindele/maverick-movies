c1455fe86c90a10e1802072aa482f5f677115a63
TheAkindele <akindelemuhyideen@gmail.com>
2020-07-28 22:15:46 +0100

    build folder

---
+++ build/precache-manifest.bb1ac375775c25228e3d77c85c14114c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ab95c5dd88c22d26afb3a54e02ce13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f76433b3166d7fd4dd36",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "72c833b18fda1e04d662",
+    "url": "/static/css/main.6936460f.chunk.css"
+  },
+  {
+    "revision": "f76433b3166d7fd4dd36",
+    "url": "/static/js/2.99d73b46.chunk.js"
+  },
+  {
+    "revision": "8debe1713c30c1c8fa79b57451984727",
+    "url": "/static/js/2.99d73b46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72c833b18fda1e04d662",
+    "url": "/static/js/main.808d6bf8.chunk.js"
+  },
+  {
+    "revision": "55e0e3644e0baf46ad63",
+    "url": "/static/js/runtime-main.b17c024b.js"
+  }
+]);
